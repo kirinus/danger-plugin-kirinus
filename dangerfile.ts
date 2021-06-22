5f48bda94d0672cef71f2a236eea5f0220a8f720
Dario Blanco <dblancoit@gmail.com>
2021-06-22 22:21:37 +0200

    chore: update dangerfile

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,10 +1,5 @@
-import { rules as conventionalRules } from '@commitlint/config-conventional';
-import commitlint from 'danger-plugin-conventional-commitlint';
 import kirinus from 'danger-plugin-kirinus';
 
-kirinus();
-
 (async function dangerReport() {
-  // FAIL if commits are not following the conventional rules
-  await commitlint(conventionalRules, { severity: 'fail' });
+  await kirinus();
 })();
